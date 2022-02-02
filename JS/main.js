6811d475093e73b349e9736a1754cb8d4a5d6525
Anatolii-code <alanwelex@gmail.com>
2022-02-02 17:53:37 +0200

    1

---
+++ JS/main.js
@@ -0,0 +1,65 @@
+//'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//console.log , alert, document.write
+
+//let number = 2000;
+//let txt1 = 'hello';
+//let txt2 = 'owu';
+//let txt3 = 'com';
+//let txt4 = 'ua';
+//
+//let number2 =  1;
+//let number3 =  10;
+//let number4 = -999;
+//let number5 = 123;
+//let number6 = 3.14;
+//let number7 = 2.7;
+//let number8 = 16;
+//
+//let tr = 'true';
+//let fl = 'false';
+//
+//console.log(number);
+//console.log(txt1,txt2,txt3,txt4);
+//alert(number2);
+//document.write(number6 + ' '+
+//    number5,number7,number8,number3,number4+ '   ');
+//document.write(tr,fl+ '   ');
+
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person
+
+//let  firstName = 'Alan';
+//let  middleName ='John';
+//let  LastName = 'Velex';
+//
+//let person = [firstName, middleName, LastName];
+//document.write(person);
+//
+//prompt("my name is:", firstName);
+//prompt("my middle name is:", middleName);
+//prompt("my age is", '22');
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+//В однакових виразах не використовувати однакові оператори!!!
+
+console.log (5 > 6);
+console.log (5 == 6);
+console.log (5 >= 6);
+
+console.log (10 == 10);
+console.log (10 >= 10);
+console.log (10 != 10);
+console.log (10 > 10);
+console.log (10 < 10);
+
+console.log (123 != 123);
+console.log (123 == 123);
+
+
+
