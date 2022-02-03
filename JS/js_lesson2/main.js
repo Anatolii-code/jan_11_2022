f4b75072624b28d6dfc5eafb0792e2fa1aba77be
Anatolii-code <alanwelex@gmail.com>
2022-02-04 01:26:03 +0200

    1

---
+++ JS/js_lesson2/main.js
@@ -0,0 +1,93 @@
+
+//1)
+// let time = Math.random() * 59;
+// console.log(time);
+// if (time<=15){
+//     prompt('1 чверть');
+// }
+// else if(time>15 && time<=30){
+//     prompt('2 чверть');
+// }
+// else if(time>30 && time<=45){
+//     prompt('3 чверть');
+// }
+// else if(time>45 && time<=60){
+//     prompt('4 чверть');
+// }
+
+// //2)
+// let day = Math.random() * 31;
+// console.log(day);
+// if (day<=10) {
+//     alert('1 декада');
+// }
+// else if(10<day<=20) {
+//     alert('2 декада');
+// }
+// else if(20<day<=31){
+//     alert('3 декада');
+// }
+
+// //3)
+//
+// let test = prompt('enter key');
+// switch (test){
+//     case 'true' :
+//         alert('Wirno');
+//         break;
+//     default :
+//         alert('Neprawylno');
+// }
+
+// //4)
+// let a = prompt('enter key');
+// if (a!=0) {
+//     document.write('Wirno');
+// }
+// else {
+//     document.write('Neprawylno');
+// }
+
+//5)
+// let week_day = prompt('enter key');
+// switch (week_day){
+//     case '1' :
+//         alert('Monday dancing');
+//         break;
+//     case '2' :
+//         alert('Thusday sleeping');
+//         break;
+//     case '3' :
+//         alert('Wensday swimming');
+//         break;
+//     case '4' :
+//         alert('Thursday reading');
+//         break;
+//     case '5' :
+//         alert('Friday writing');
+//         break;
+//     case '6' :
+//         alert('Saturday relaxing');
+//         break;
+//     default :
+//         alert('Sunday ordering');
+// }
+
+////6)
+// let year = Math.random() * 2022;
+// Math.floor(year); // функція заокруглення
+// if (year%4===0){ //остача = 0
+//     alert('Wysokosnyj');
+// }
+// else {
+//     alert('Ne wysokosnyj');
+// }
+
+// //7)
+// let name = prompt('Яка <<офіційна>> назва JavaScript?');
+// if (name === 'ECMAScript'){
+//     alert('Правильно!')
+// }else {
+//     alert(' Не знаэте? ECMAScript!');
+// }
+
