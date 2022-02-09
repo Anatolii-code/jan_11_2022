08d7751d400fecd661388a7b9ef4df1b61c41219
Anatolii-code <alanwelex@gmail.com>
2022-02-09 15:53:58 +0200

    1

---
+++ JS/js_lesson3/main.js
@@ -0,0 +1,66 @@
+// 1)
+// let arrNum =  [ 1, 2, 3, 4, 5]
+// document.write(arrNum);
+
+//2)
+// let arrSt =  [ 'l3', 'qwe', 'asd', 'zcx', 'fhg'];
+// document.write(arrSt);
+// console.log(arrSt);
+
+// //3)
+// let arrBool =  [ true, false, true, true, false,
+//                  'l3', 'qwe', 'asd', 'zcx', 'fhg',
+//                  1, 2, 3, 4, 5];
+// document.write(arrBool);
+
+// //3)
+// let arrBool =  [ true, false, true, true, false,
+//                  'l3', 'qwe', 'asd', 'zcx', 'fhg',
+//                  1, 2, 3, 4, 5];
+// document.write(arrBool);
+
+// //4)
+// let arrVoid =  [];
+// arrVoid[0] = true;
+// arrVoid[1] = 1;
+// arrVoid[2] = 2;
+// arrVoid[3] = 'qweqwe';
+// arrVoid[4] = 'qweqwe';
+// arrVoid[5] = '123';
+//
+// document.write(arrVoid);
+
+//4)
+// let arrVoid =  [];
+// arrVoid[0] = true;
+// arrVoid[1] = 1;
+// arrVoid[2] = 2;
+// arrVoid[3] = 'qweqwe';
+// arrVoid[4] = 'qweqwe';
+// arrVoid[5] = '123';
+// //
+// document.write(arrVoid);
+
+// //5/6)
+// document.write('<ol class="arrVoidClass">')
+// for (let i = 0; i<10 ; i++ ) {
+//
+//     document.write(i);
+//     document.write('<li>${arrVoid}</li>');
+//
+// }
+// document.write('</ol>');
+
+// //7)- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// let i = 0;
+// while (i<20){
+//     document.write('<h1>Lorem ipsum dolor.</h1>');
+//     i++;
+// }
+
+//8)
+let i = 0;
+while (i<20){
+    document.write('<h1>Lorem ipsum dolor.</h1>');
+    i++;
+}
